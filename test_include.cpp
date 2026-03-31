518117dcde15ad6d52ca3ebdebb1824b24a90996
Inverse <15045523020@163.com>
2026-03-31 16:08:37 +0800

    算是在PC上配置好了easyX

---
+++ test_include.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+// 测试编译器包含路径
+int main() {
+    // 打印一些预定义宏，查看编译器信息
+    std::cout << "Compiler version: " << _MSC_VER << std::endl;
+    std::cout << "Include paths:" << std::endl;
+    
+    // 尝试包含 graphics.h
+    #ifdef __has_include
+        #if __has_include(<graphics.h>)
+            std::cout << "graphics.h found!" << std::endl;
+        #else
+            std::cout << "graphics.h NOT found!" << std::endl;
+        #endif
+        #if __has_include(<iostream>)
+            std::cout << "iostream found!" << std::endl;
+        #else
+            std::cout << "iostream NOT found!" << std::endl;
+        #endif
+    #endif
+    
+    return 0;
+}
