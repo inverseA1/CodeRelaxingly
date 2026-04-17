e72b8be15aee9d368e9c5e545ad3070548faf725
Inverse <15045523020@163.com>
2026-04-17 11:46:19 +0800

    下课

--- Iteration.cpp
+++ Iteration.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <iomanip>
 #define ERROR 0.000000000001 // 定义误差
 #define EPSILON 0.00001
 
@@ -10,12 +12,29 @@ inline double FindDfun(double x, double (*func)(double), double epsilon) {
     return (func(x + epsilon) - func(x - epsilon)) / 2*epsilon ;
 }
 
+double Polynomial(std::vector<double> coefficient, double x){
+    double result = 0;
+    int n = coefficient.size();
+    for(int i = 0 ; i < n ; i++){
+        result += coefficient[i] * std::pow(x, i);
+    }
+    return result;
+}
+
+double Dpolynomial(std::vector<double> coefficient, double x){
+    double result = 0;
+    int n = coefficient.size();
+    for(int i = 1 ; i < n ; i++){
+        result += coefficient[i] * i * std::pow(x, i-1);
+    }
+    return result;
+}
+
 double Leration(double error, double x){
-    
     double x_ = x - Func(x) / Dfun(x) ;
-    std::cout<< "Func(x)=" << Func(x) << " Dfun(x)=" << Dfun(x)
-             << " x=" << x << " x_=" << x_ << " error=" << abs(x_ - x) <<std::endl;
-    if(abs(x_ - x) <= error){
+    //std::cout<< "Func(x)=" << Func(x) << " Dfun(x)=" << Dfun(x)
+             //<< " x=" << x << " x_=" << x_ << " error=" << std::abs(x_ - x) <<std::endl;
+    if(std::abs(x_ - x) <= error){
         return x_;
     }else{
         return Leration(error, x_);
@@ -24,16 +43,30 @@ double Leration(double error, double x){
 
 double Leration_(double error, double x){
     double x_ = x - Func(x) / FindDfun(x, Func, EPSILON) ;
-    std::cout << "x = " << x << " & " << "x_ = " << x_ << " error = " << abs(x_ - x) <<std::endl;
-    if(abs(x_ - x) <= error){
+    std::cout << "x = " << x << " & " << "x_ = " << x_ << " error = " << std::abs(x_ - x) <<std::endl;
+    if(std::abs(x_ - x) <= error){
         return x_;
     }else{
         return Leration_(error, x_);
     }
 }
 
+double LerationPoly(double error, double x,std::vector<double> coefficient){
+    double x_ = x - Polynomial(coefficient, x) / Dpolynomial(coefficient, x) ;
+        //std::cout<< "Polynomial(x)=" << Polynomial(coefficient,x) << " Dpolynomial(x)=" << Dpolynomial(coefficient,x)
+             //<< " x=" << x << " x_=" << x_ << " error=" << std::abs(x_ - x) <<std::endl;
+    if(std::abs(x_ - x) <= error){
+        return x_;
+    }else{
+        return LerationPoly(error, x_,coefficient);
+    }
+}
+
 int main(){
-    std::cout << Leration(ERROR, 3.1415926) << std::endl;
-    //std::cout << Leration_(ERROR, 3) << std::endl;
+    std::vector<double> coeff = {-10, 0, 1};
+    std::cout << std::setprecision(12);
+    //std::cout << Leration(ERROR, 1) << std::endl;
+    //std::cout << Leration_(ERROR, 1) << std::endl;
+    std::cout << LerationPoly(ERROR, 3, coeff) << std::endl;
     return 0;
 }
